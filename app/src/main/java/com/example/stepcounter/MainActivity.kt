72f6d633e17a944a148bfb8207fe4217d9337fe7
SzaniszloGabor <gaborsz234@gmail.com>
2021-11-30 22:28:24 +0100

    feat: add today's steps counter
    Step counter that resets every day

--- app/src/main/java/com/example/stepcounter/MainActivity.kt
+++ app/src/main/java/com/example/stepcounter/MainActivity.kt
@@ -12,25 +12,36 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import java.util.*
 import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
     private var sensorManager: SensorManager? = null
     private var MagnitudePrevious = 0f
+
     private var running = false
+
     private var currentSteps = 0
     private var totalSteps = 0
     private var previousTotalSteps = 0
+    private var previousTodaysSteps = 0
+    private var stepsToday = 0
+
+    private var cal = Calendar.getInstance()
+    private var currentDate = cal.get(Calendar.DAY_OF_YEAR)
+
     private lateinit var textView: TextView
     private lateinit var button: Button
     private lateinit var totalTextView: TextView
+    private lateinit var stepsTodayView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         textView = findViewById(R.id.stepsTaken)
         button = findViewById(R.id.button)
         totalTextView = findViewById(R.id.totalSteps)
+        stepsTodayView = findViewById(R.id.stepsToday)
         resetSteps()
         loadData()
 
@@ -65,7 +76,9 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         }
         MagnitudePrevious = Magnitude
         currentSteps = totalSteps - previousTotalSteps
-        textView.text = (currentSteps).toString();
+        stepsToday = totalSteps - previousTodaysSteps
+        stepsTodayView.text = stepsToday.toString()
+        textView.text = currentSteps.toString();
         totalTextView.text = totalSteps.toString()
         saveData()
     }
@@ -114,7 +127,9 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
         val editor = sharedPreferences.edit()
         editor.putInt("key1", totalSteps)
-        //editor.putInt("key2", previousTotalSteps)
+        editor.putInt("key2", stepsToday)
+        editor.putInt("DATE_KEY", currentDate)
+
         editor.apply()
     }
 
@@ -123,12 +138,21 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         // In this function we will retrieve data
         val sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
         val savedNumber = sharedPreferences.getInt("key1", 0)
-        //val prevStepNumber = sharedPreferences.getInt("key2", 0)
+
+        val savedDate = sharedPreferences.getInt("DATE_KEY",0)
+        val todayStepNumber = sharedPreferences.getInt("key2", 0)
         // Log.d is used for debugging purposes
         Log.d("MainActivity", "$savedNumber")
-
+        Log.d("CURRENT DATE ", "$currentDate")
+        Log.d("SAVED DATE", "$savedDate")
+        if (currentDate != savedDate) {
+            previousTodaysSteps = todayStepNumber
+            stepsToday = 0
+            stepsTodayView.text = 0.toString()
+        }
         totalSteps = savedNumber
         previousTotalSteps = totalSteps
+
     }
 
 }
